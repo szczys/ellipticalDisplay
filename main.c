ba63eae45ee202e69359b34631bcb1e0e7cd18e8
Mike Szczys <szczys@hotmail.com>
2017-01-22 11:28:45 -0600

    Implemented basic SPI slave with interrupts

--- main.c
+++ main.c
@@ -13,6 +13,10 @@
 #include <avr/pgmspace.h>
 #include <util/delay.h>
 
+volatile uint8_t SShighFlag = 1;
+volatile uint8_t bufferIDX = 0;
+volatile uint8_t spiRxBuffer[20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
 /**************** Prototypes *************************************/
 void init_IO(void);
 void init_interrupts(void);
@@ -20,8 +24,13 @@ int main(void);
 /**************** End Prototypes *********************************/
 
 void init_IO(void){
-    DDRB |= 1<<PB0;
-    PORTB |= 1<<PB0;
+    DDRB |= 1<<PB0 | 1<<PB1;
+    PORTB |= 1<<PB0 | 1<<PB1;
+}
+
+void init_SPI(void){
+    //Slave using interrupts
+    SPCR |= (1<<SPE) | (1<<SPIE) | (1<<CPOL) | (1<<CPHA);
 }
 
 void init_interrupts(void) {
@@ -34,23 +43,39 @@ void init_interrupts(void) {
     //Timer overflow for debounce and systick
     TCCR0B |= 1<<CS02 | 1<<CS00;		//Divide by 1024
     TIMSK0 |= 1<<TOIE0;			//enable timer overflow interrupt
-
-    sei();
     */
+    sei();
+
 }
 
 int main(void)
 {
     init_IO();
+    init_SPI();
     init_interrupts();
     //_delay_ms(200);
-
-
+    //SPSR = 1<<SPIF;
+    
     while(1)
     {
-       PINB |= 1<<PB0;
-      _delay_ms(200);
+        if (SShighFlag == 0) {
+            if (PINB & (1<<PB2)) {
+                //Interrupt routine set the flag low
+                //But SS is now high. Unset flag and reset buffer index
+                SShighFlag == 1;
+                bufferIDX = 0;
+            }
+        }
+        if (spiRxBuffer[1] == 0xF0) {
+            PORTB ^= 1<<PB1;
+        }
     }
+}
+
+ISR(SPI_STC_vect) {
+    SShighFlag = 0;
+    spiRxBuffer[bufferIDX++] = SPDR;
+    //TODO: Handle buffer overflow condition
 }
 
 /*
